bb1a1fd34ca8e4ddb589604243e4ce50ea2fee17
Manish kumawat <m31122004k@gmail.com>
2026-01-18 22:54:22 +0530

    fix image issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    loader: 'custom',
-    loaderFile: './loader.js',
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.evbuc.com"
-      }
-    ]
-  }
+      { protocol: "https", hostname: "img.evbuc.com" },
+    ],
+  },
 };
 
 export default nextConfig;
